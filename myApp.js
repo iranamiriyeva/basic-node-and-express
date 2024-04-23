b97760a0e42ab02ebadb100db342e602b1d36202
Irana Miriyeva <irana.miriyeva@gmail.com>
2024-04-23 08:02:13 +0000

    Solution4: Serve Static Assets

--- myApp.js
+++ myApp.js
@@ -1,15 +1,25 @@
-let express = require('express');
-let app = express();
+let express = require('express')
+let app = express()
+const path = require('path')
 
+//Solution2: Start a Working Express Server
 // app.get("/", (req, res) => {
-//     res.send("Hello Express");
-// });
+//     res.send("Hello Express")
+// })
 
-let absolutePath = __dirname + '/views/index.html'
+//Solution3: Serve an HTML File
+// let absolutePath = __dirname + '/views/index.html'
+// app.get("/", (req, res) => {
+//     res.sendFile(absolutePath)
+// })
 
-app.get("/", (req, res) => {
-    res.sendFile(absolutePath);
-});
+//Solution4: Serve Static Assets
+app.use('/public', express.static(path.join(__dirname, 'public')))
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'))
+})
+
+
 
 
 
